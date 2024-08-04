9e322d89ab7d9fdd434480fe7c6974add4815460
Your Name <you@example.com>
2024-08-04 23:55:17 +0800

    test

---
+++ cnd.ts
@@ -0,0 +1,16 @@
+export default {
+  "entity": {
+    "id": "cnd",
+    "name": "文革博物馆",
+    "internal": false,
+    "type": "db",
+    "official": false,
+    "author": ""
+  },
+  "parser_option": {
+  },
+  "parser_id": "CND",
+  "path": "",
+  "resource_type": "book",
+  "version": 2
+}
